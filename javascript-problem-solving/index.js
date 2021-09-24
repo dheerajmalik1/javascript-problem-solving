4e10ac87436b853b60fb25a2ebdf3d414e1a23ad
dheerajmalik1 <dheerajmalikwork@gmail.com>
2021-09-24 15:06:39 +0530

    added questions and started with solutions

--- javascript-problem-solving/index.js
+++ javascript-problem-solving/index.js
@@ -1,2 +1,48 @@
-console.log(0);
-//checking again
+function cl(value) {
+      console.log(value);
+}
+
+
+/**
+ * fizz buzz
+ * 2 sum
+ * remove all duplicates from an array
+ * convert array to string 
+ * convert string to array
+ * find count of each unique alphabets in a string for eg - missisippi
+ * Check if the string has any repeating character within it, if yes return true else return false(isogram?)
+ * check if the given array has unique integers or not
+ * 
+ */
+
+
+
+/**
+ * check if the given array has unique integers or not
+ */
+
+function checkUniqueIntegerArray(arr) {
+
+      arr.map(ele => {
+            console.log(ele)
+      });
+}
+
+/**
+ * to be done
+ */
+
+
+
+/**
+ * remove all duplicate entries in the array
+ * for eg let arr = [{id:1, name:"dhee"},{id:2,name:"popo"},{id:1, name:"dhee"}]
+ */
+
+function removeDuplicateEntries(arr) {
+      cl(arr);
+}
+
+let arrR = [{ id: 1, name: "dhee" }, { id: 2, name: "popo" }, { id: 1, name: "dhee" }];
+
+removeDuplicateEntries(arrR);
